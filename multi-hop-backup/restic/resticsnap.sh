a9f29e6df7423ff71dc2484bd6e0e578455d3c4c
abig4m <miguef10hd@gmail.com>
2025-06-08 13:41:36 -0500

    add restic backup

---
+++ multi-hop-backup/restic/resticsnap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function ctrl_c (){
+	echo -e "End Process"
+	exit 1
+}
+
+trap ctrl_c INT
+
+namerepo=/yournamerepo  #name of you reposiry, example ./restic0 or /mnt/diskA/resctic0
+backup_directory=/yourbackupdirectory #direcotory to backup, example /mnt/disk500 or /home/user or /etc
+
+echo ">>>>>><<<<<<------ init restic backup ------>>>>>><<<<<"
+echo "$(date)"
+
+# backup restic
+
+restic -r $namerepo --verbose backup /mnt/disk500GB --password-file /root/keyfiler0
+restic -r $namerepo --verbose backup /home/mig/ --password-file /root/keyfiler0
+restic -r $namerepo --verbose backup /etc --password-file /root/keyfiler0
+echo ">>>>><<<<<<<----- backup finish $(date)"
+
+
+
+
